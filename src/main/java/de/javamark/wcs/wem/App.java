ac1c46c6eaf75483300277ec21995141075a2bc5
Mark Goericke <mark@javamark.de>
2015-08-05 08:57:40 +0200

    kommentare in klassen

--- src/main/java/de/javamark/wcs/wem/App.java
+++ src/main/java/de/javamark/wcs/wem/App.java
@@ -3,11 +3,17 @@ package de.javamark.wcs.wem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Import;
 
+/**
+ * Starter Class - pom.xml: <start-class>de.javamark.wcs.wem.App</start-class>
+ * 
+ * @author mark
+ *
+ */
 @Import(AppConfig.class)
 public class App {
 
     public static void main(String[] args) throws Exception {
-    	// EhCache mag keine IPv6 Adressen :( - Sollte ein Fehler `EhCache: cannot assign requested address
+    	// EhCache mag keine IPv6 Adressen :( - Fehler ist `EhCache: cannot assign requested address, Lšsung ist folgende ...
     	System.getProperties().setProperty("java.net.preferIPv4Stack", "true");
     	
         SpringApplication.run(App.class, args);
